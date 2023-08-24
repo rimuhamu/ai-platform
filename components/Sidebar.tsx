d5f3768e2b6d821697e60c2b27198439184500d6
Emir Risyad <emirrsyd@gmail.com>
2023-08-25 00:44:33 +0200

    api limit UI

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -15,6 +15,7 @@ import {
   Settings,
   VideoIcon
 } from 'lucide-react';
+import { FreeCounter } from './FreeCounter';
 
 const montserrat = Montserrat({ weight: '600', subsets: ['latin'] });
 
@@ -63,7 +64,11 @@ const routes = [
   }
 ];
 
-const Sidebar = () => {
+interface SidebarProps {
+  apiLimitCount: number;
+}
+
+const Sidebar = ({ apiLimitCount = 0 }: SidebarProps) => {
   const pathname = usePathname();
   return (
     <div className="space-y-4 py-4 flex flex-col h-full bg-[#111827] text-white">
@@ -96,6 +101,7 @@ const Sidebar = () => {
           ))}
         </div>
       </div>
+      <FreeCounter apiLimitCount={apiLimitCount} />
     </div>
   );
 };
