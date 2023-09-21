099975be3c2626bfaadd9e4f2a408131ab7ad90b
Emir Risyad <emirrsyd@gmail.com>
2023-09-21 23:06:40 +0200

    stripe integration

--- components/ProModal.tsx
+++ components/ProModal.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import axios from 'axios';
+
 import { useProModal } from '@/hooks/useProModal';
 
 import {
@@ -25,6 +27,7 @@ import { Badge } from './ui/badge';
 import { Card } from './ui/card';
 import { cn } from '@/lib/utils';
 import { Button } from './ui/button';
+import { useState } from 'react';
 
 const tools = [
   {
@@ -61,6 +64,20 @@ const tools = [
 
 export const ProModal = () => {
   const proModal = useProModal();
+  const [loading, setLoading] = useState(false);
+
+  const onSubscribe = async () => {
+    try {
+      setLoading(true);
+      const response = await axios.get('/api/stripe');
+
+      window.location.href = response.data.url;
+    } catch (error) {
+      console.log(error, 'STRIPE_CLIENT_ERROR');
+    } finally {
+      setLoading(false);
+    }
+  };
 
   return (
     <Dialog open={proModal.isOpen} onOpenChange={proModal.onClose}>
@@ -92,7 +109,12 @@ export const ProModal = () => {
           </DialogDescription>
         </DialogHeader>
         <DialogFooter>
-          <Button size="lg" variant="premium" className="w-full">
+          <Button
+            onClick={onSubscribe}
+            size="lg"
+            variant="premium"
+            className="w-full"
+          >
             Upgrade
             <Zap className="w-4 h-4 ml-2 fill-white" />
           </Button>
